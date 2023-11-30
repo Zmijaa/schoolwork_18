713e25f2680b3b6c1a6f576d1be773081f2bf2c2
Filip Mircev <147530688+Zmijaa@users.noreply.github.com>
2023-11-30 09:19:54 +0100

    Add files via upload

---
+++ Project_18/src/promenliva_na_celi_broj.java
@@ -0,0 +1,27 @@
+
+public class promenliva_na_celi_broj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a = 5; 
+	        int b = 10; 
+	        int c = 3; 
+	        int k;
+	        int d;
+
+	        if (a <= 5 && b > (a - c)) {
+	            k = 1;
+	            System.out.println("k = 1");
+	        } else {
+	            d = 0;
+	            System.out.println("d = 0");
+
+	
+		
+
+	
+
+	}
+
+	}
+}
